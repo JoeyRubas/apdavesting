428cdf8a3226c9b80b2ec7ad64f6ea6798f4f310
JoeyRubas <joeymrubas@gmail.com>
2025-01-29 00:40:29 -0500

    database and libs

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,14 +1,16 @@
 import os
 import sys
 
-# Add the project directory to the system path
-sys.path.append('/home/joey/apdavesting')
+# Get project and virtual environment paths from environment variables
+PROJECT_PATH = os.getenv("DJANGO_PROJECT_PATH", os.path.dirname(os.path.abspath(__file__)))
+VENV_PATH = os.getenv("DJANGO_VENV_PATH", os.path.join(PROJECT_PATH, "venv/lib/python3.12/site-packages"))
 
-# Add the virtual environment’s site-packages
-sys.path.append('/home/joey/apdavesting/venv/lib/python3.12/site-packages')
+# Add project and virtual environment paths to sys.path
+sys.path.append(PROJECT_PATH)
+sys.path.append(VENV_PATH)
 
-# Ensure the correct settings module is used
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+# Set Django settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 from django.core.wsgi import get_wsgi_application
 
